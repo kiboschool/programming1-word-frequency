00aa593240ddeecbf26f98b2d0b3113d70871811
Rob Cobb <rwcobbjr@gmail.com>
2022-10-12 13:09:13 +0100

    read from file

--- test_main.py
+++ test_main.py
@@ -3,39 +3,12 @@ from main import *
 
 # Retribution, Ida B. Luckie
 # https://opinion.premiumtimesng.com/2018/10/19/sol-plaatje-prophesying-the-future-of-his-dreams-by-kole-omoto%E1%B9%A3o/
-poem = """
-    Alas, My Country!  Thou wilt have no need
-    Of enemy to bring thee to thy doom . . .
-    For not alone by war a nation falls.
-    Though she be fair, serene as radiant morn,
-    Though girt by seas, secure in armament,
-    Let her but spurn the vision of the Cross;
-    Tread with contemptuous feet on its command
-    Of mercy, Love and Human Brotherhood,
-    And she, some fateful day, shall have no need
-    Of enemy to bring her to the dust.
-
-    Some day, though distant it may be -- with God
-    A thousand years are but as yesterday --
-    The germs of hate, injustice, violence,
-    Like an insidious canker in the blood,
-    Shall eat that nation's vitals.  She shall see
-    Break forth the blood-red tide of anarchy,
-    Sweeping her plains, laying her cities low,
-    And bearing on its seething, crimson flood
-    The wreck of Government, of home, and all
-    The nation's pride , its splendour and its power.
-    On with relentless flow, into the seas
-    Of God's eternal vengeance wide and deep.
-    But, for God's grace!  Oh may it hold thee fast,
-    My Country, until justice shall prevail
-    O'er wrong and o'er oppression's cruel power,
-    And all that makes humanity to mourn.
-"""
+with open("poem.txt") as f:
+    POEM = f.read()
 
 class Test(unittest.TestCase):
     def test_word_frequency(self):
-        frequencies = word_frequency(poem)
+        frequencies = word_frequency(POEM)
         self.assertEqual(frequencies["pride"], 1)
         self.assertEqual(frequencies["be"], 2)
         self.assertEqual(frequencies["the"], 6)
